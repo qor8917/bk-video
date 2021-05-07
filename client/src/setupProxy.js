ede5ef27d57684852f4e7040c108f364fab4acb0
qor8917 <qor8917@gmail.com>
2021-05-07 18:26:47 +0900

    ..

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
+    createProxyMiddleware('/api', {
+      target: 'http://3.35.64.50/:5000',
       changeOrigin: true,
     })
   );
--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const connect = mongoose
   })
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
-
+// CORS Middleware
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,11 +31,17 @@ app.use('/api/video', require('./routes/video'));
 app.use('/api/subscribe', require('./routes/subscribe'));
 app.use('/api/comment', require('./routes/comment'));
 
+//use this to show static files you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
   app.use(express.static('client/build'));
 
+  // index.html for all page routes  html or routing and naviagtion
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
